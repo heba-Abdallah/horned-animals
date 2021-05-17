95ec631baf60a91ad1fc2d707fa666e0944710a8
heba-Abdallah <heba.essam.m@hotmail.com>
2021-05-17 20:01:57 +0300

    doing lab 2

--- src/components/main.js
+++ src/components/main.js
@@ -5,19 +5,13 @@ import hornedData from './HornedData.json';
 
 class Main extends React.Component {
 
-    // constructor(props){
-    //     super(props);
-    //     this.state ={
-    //         name: props.titel
-    //     }
-    
-
     render() {
         return (
             <>
-                {hornedData.map(item => {
+                {hornedData.map((item, index) => {
                     return (
                         <HornedBeast
+                            key={index}
                             titel={item.title}
                             imageUrl={item.image_url}
                             description={item.description} />
@@ -29,16 +23,3 @@ class Main extends React.Component {
 }
 
 export default Main;
-
-{/* <HornedBeast
-                 titel={'UniWhal'} 
-                 imageUrl={"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"}  
-                 description={'A unicorn and a narwhal nuzzling their horns'}/>
-                <HornedBeast
-                 titel={'Rhino Family'} 
-                 imageUrl={"https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"} 
-                 description={"Mother (or father) rhino with two babies"}/>
-                <HornedBeast
-                 titel={"Unicorn Head"}
-                  imageUrl={"https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"}
-                   description={'Someone wearing a creepy unicorn head mask'}/> */}
